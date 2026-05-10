a3d6e38e57f6a9258de59196e95760c693c2cd2e
Deny Esquerre <lular@ucvvirtual.edu.pe>
2026-05-10 07:10:22 -0500

    feat: aumentar logo login a w-40

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -79,7 +79,7 @@ export function LoginForm({
               <img 
                 src={logoUrl} 
                 alt="FishFlow Logo" 
-                className="w-32 h-auto object-contain"
+                className="w-40 h-auto object-contain"
               />
               <span className="sr-only">FishFlow</span>
             </a>
